8e11bcb04dd20db168499171df77f81e44e23857
VitMexia <45263788+VitMexia@users.noreply.github.com>
2023-05-08 22:54:30 +0100

    A square

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import Display from "./Display";
+// import ButtonPanel from "./ButtonPanel";
+// import calculate from "../logic/calculate";
+// import "./App.css";
+import "./css/bootstrap.min.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  
+  state = {
+    total: null,
+    next: null,
+    operation: null,
+  };
+
+  // handleClick = buttonName => {
+  //   this.setState(calculate(this.state, buttonName));
+  // };
+
+  render() {
+    return (
+      // <div class="container">
+      //   <div  style={{height: "100%", width: "90%" }}>
+      //   HELLO WORLD
+      //   </div>
+      //   {/* <Display value={this.state.next || this.state.total || "0"} />
+      //   <ButtonPanel clickHandler={this.handleClick} /> */}
+      // </div>
+
+      //<div style={{height: "vh-100", backgroundcolor: "rgba(255,0,0,.1)"}}>
+      <div class="container vh-100">
+        <div class="mh-100 mt-3 border border-warning" style={{width: '90%', height: '90%', backgroundcolor: 'rgba(0,0,255,.1)'}}>Max-height 100%</div>
+      </div>
+    );
+  }
 }
-
-export default App;
